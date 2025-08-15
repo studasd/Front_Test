fc4647875d6067224b9f517d0a67985a6846bf82
fuser85 <fuser85@myoffice365.site>
2025-08-15 23:15:10 +0300

    props

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
 
-function App() {
+type Props = {
+  value: number;
+};
+
+function Square({ value }: Props) {
+  function handleClick() {
+    console.log("click");
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
+}
+
+export function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
