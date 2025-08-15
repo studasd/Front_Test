f7d6441be12f582cfd8756029c0c4b7aef278152
fuser85 <fuser85@myoffice365.site>
2025-08-16 01:10:13 +0300

    added O

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,12 +16,24 @@ function Square({ value, onSquareClick }: Props) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
 
   function hanldeClick(i: number) {
+    if (squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
